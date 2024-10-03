76a07a3e9946c0e7706b0894789ca1a4d4b57358
Hau Nguyen <hau.nguyen@decision-science.com>
2024-10-03 13:43:07 +0700

    update code

--- GenerateJWKS/App.cs
+++ GenerateJWKS/App.cs
@@ -1,6 +1,7 @@
 ﻿using GenerateJWKS.Repositories.Interfaces;
 using Jose;
 using System.Security.Cryptography;
+using System.Text;
 
 namespace GenerateJWKS
 {
@@ -8,10 +9,13 @@ namespace GenerateJWKS
     {
         private readonly IJwkGenerator _jwkGenerator;
 
-        private readonly string _certFolder = $"Certs";
+        private readonly string _cert = $"Certs";
 
-        private readonly string _filePublicKeys = $"oidc-spcp-public.json";
-        private readonly string _filePrivateKeys = $"oidc-spcp-secret.json";
+        private readonly string _filePublicKeys = $"oidc-jwks-public.json";
+        private readonly string _filePrivateKeys = $"oidc-jwks-private.json";
+
+        private readonly string _filePrivateSigKeyPem = $"oidc-sig-private";
+        private readonly string _filePrivateEncKeyPem = $"oidc-enc-private";
         public App(IJwkGenerator jwkGenerator)
         {
             _jwkGenerator = jwkGenerator;
@@ -19,18 +23,42 @@ namespace GenerateJWKS
 
         public async Task RunAsync()
         {
+            string kidSig = string.Empty;
+            string kidEnc = string.Empty;
 
-            await GenerateJwkSets();
+            while (string.IsNullOrEmpty(kidSig))
+            {
+                Console.Write("Please enter signature key id: ");
+                kidSig = Console.ReadLine();
+            }
+
+            while (string.IsNullOrEmpty(kidEnc))
+            {
+                Console.Write("Please enter encrypt key id: ");
+                kidEnc = Console.ReadLine();
+            }
+
+            await GenerateJwkSets(kidSig, kidEnc);
+            Console.Write("Generate JWKS completed.");
+            Console.ReadLine();
+
             await Task.CompletedTask;
         }
 
-        private async Task GenerateJwkSets()
+        private async Task GenerateJwkSets(string kidSig, string kidEnc)
         {
             try
             {
+                var directory = Directory.GetCurrentDirectory();
+                var certFolder = Path.Combine(directory, _cert);
+                if (!Directory.Exists(certFolder))
+                {
+                    Directory.CreateDirectory(certFolder);
+                }
+
                 //generate key pairs
-                var keySignature = _jwkGenerator.GenerateEcdsaKeyPairs(ECCurve.NamedCurves.nistP256, Enums.EcdsaAlgorithm.ES256, "oidc-sig-pub");
-                var keyEncrypt = _jwkGenerator.GenerateEcdsaKeyPairs(ECCurve.NamedCurves.nistP256, Enums.EcdsaAlgorithm.EcdhEsA128kw, "oidc-enc-pub");
+                var keySignature = _jwkGenerator.GenerateEcdsaKeyPairs(ECCurve.NamedCurves.nistP256, Enums.EcdsaAlgorithm.ES256, kidSig);
+                var keyEncrypt = _jwkGenerator.GenerateEcdsaKeyPairs(ECCurve.NamedCurves.nistP256, Enums.EcdsaAlgorithm.EcdhEsA256kw, kidEnc);
 
                 //create key set
                 var listPrivateKeys = new List<Jose.Jwk>() { keySignature.Item1, keyEncrypt.Item1 };
@@ -39,9 +67,23 @@ namespace GenerateJWKS
                 var listPublicKeys = new List<Jose.Jwk>() { keySignature.Item2, keyEncrypt.Item2 };
                 var publicKeySet = new Jose.JwkSet(listPublicKeys);
 
+                //write pem key to file
+                byte[] privateKeySig = keySignature.Item3.ExportECPrivateKey();
+                string privateKeySigPem = ConvertDERToPEM(privateKeySig, "EC PRIVATE KEY");
+
+                byte[] privateKeyEnc = keyEncrypt.Item3.ExportECPrivateKey();
+                string privateKeyEncPem = ConvertDERToPEM(privateKeyEnc, "EC PRIVATE KEY");
+
+                var pathPrivateSigPem = $"{certFolder}/{_filePrivateSigKeyPem}";
+                var pathPrivateEncPem = $"{certFolder}/{_filePrivateEncKeyPem}";
+
+                await File.WriteAllTextAsync(pathPrivateSigPem, privateKeySigPem);
+                await File.WriteAllTextAsync(pathPrivateEncPem, privateKeyEncPem);
+
                 //write key set to file
-                var pathPrivateCert = $"{_certFolder}/{_filePrivateKeys}";
-                var pathPublicCert = $"{_certFolder}/{_filePublicKeys}";
+                var pathPrivateCert = $"{certFolder}/{_filePrivateKeys}";
+                var pathPublicCert = $"{certFolder}/{_filePublicKeys}";
+
                 await WriteToFile(privateKeySet, pathPrivateCert);
                 await WriteToFile(publicKeySet, pathPublicCert);
             }
@@ -51,7 +93,7 @@ namespace GenerateJWKS
             }
         }
 
-        private async Task WriteToFile(JwkSet keySet, string pathCert)
+        private async static Task WriteToFile(JwkSet keySet, string pathCert)
         {
             using (FileStream fileStream = new(pathCert, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None))
             {
@@ -62,6 +104,15 @@ namespace GenerateJWKS
                     await writer.WriteLineAsync(keys);
                 }
             }
+        }
+
+        private static string ConvertDERToPEM(byte[] der, string pemLabel)
+        {
+            StringBuilder pemBuilder = new StringBuilder();
+            pemBuilder.AppendLine($"-----BEGIN {pemLabel}-----");
+            pemBuilder.AppendLine(Convert.ToBase64String(der, Base64FormattingOptions.InsertLineBreaks));
+            pemBuilder.AppendLine($"-----END {pemLabel}-----");
+            return pemBuilder.ToString();
         }
     }
 }
